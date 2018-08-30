98ef669d6928f37260f781a1215c29dd42654d9d
Ezra Savard <ezsavard@microsoft.com>
2018-08-30 12:29:17 -0700

    add integration test coverage for Spark versions 1.6 and 2.0

---
+++ integration-test/spark/v2_0/src/test/java/com/microsoft/azure/datalake/store/telemetry/TestJobInfoProviderSpark.java
@@ -0,0 +1,50 @@
+package com.microsoft.azure.datalake.store.telemetry;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class TestJobInfoProviderSpark {
+    private static final Logger LOG =
+            LoggerFactory.getLogger(TestJobInfoProviderSpark.class);
+
+    @Before
+    public void setup() throws NoSuchFieldException, IllegalAccessException {
+        // reset singleton
+        Field instance = JobInfoProvider.class.getDeclaredField("instance");
+        instance.setAccessible(true);
+        instance.set(null, null);
+    }
+
+    @Test
+    public void testJobIdProviderWorksWithSparkContextInitialized() {
+        SparkConf conf = new SparkConf()
+                .setAppName("Lucky App")
+                .setMaster("local")
+                .set("spark.driver.bindAddress", "127.0.0.1");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        List<String> data = Arrays.asList("Hearts", "Stars", "Horseshoes");
+        JavaRDD<String> logData = sc.parallelize(data);
+        LOG.info("Test data: " + logData.toString());
+        JobInfoProvider x = JobInfoProvider.get();
+        assertEquals("spark", x.getEngineName());
+        assertNotNull(x.getApplicationId());
+    }
+
+    @Test
+    public void testJobIdProviderWorksWithNoSparkContext() {
+        JobInfoProvider x = JobInfoProvider.get();
+        assertNull(x.getEngineName());
+        assertNull(x.getApplicationId());
+    }
+}
